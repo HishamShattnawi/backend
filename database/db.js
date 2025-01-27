a68caa8dd8ec3d401e3d3f54b0fd76f3fc0daf5f
HishamShattnawi <h.shattnawi.2002@gmail.com>
2025-01-27 14:41:34 +0300

    Initial commit

--- database/db.js
+++ database/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Database = require("better-sqlite3");
 const path = require("path");
 
@@ -7,13 +6,3 @@ const dbPath = path.join(__dirname, "my_db.db");
 const db = new Database(dbPath);
 
 module.exports = db;
-=======
-const Database = require("better-sqlite3");
-const path = require("path");
-
-const dbPath = path.join(__dirname, "my_db.db");
-
-const db = new Database(dbPath);
-
-module.exports = db;
->>>>>>> 5cdca4e56534d58dd612fab75eeb15af0a4fd28b
--- database/getRows.js
+++ database/getRows.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const db = require('./db');
 
 
@@ -17,23 +16,3 @@ function getRows(tableName , searchQuery) {
 }
 
 module.exports = getRows;
-=======
-const db = require('./db');
-
-
-function getRows(tableName , searchQuery) {
-  const query = `SELECT * FROM ${tableName} ${searchQuery}`;
-
-  try {
-    const stmt = db.prepare(query);
-    const rows = stmt.all();
-    console.log(`Retrieved rows from ${tableName}.`);
-    return rows;
-  } catch (err) {
-    console.error(`Error retrieving rows from ${tableName}:`, err.message);
-    return [];
-  }
-}
-
-module.exports = getRows;
->>>>>>> 5cdca4e56534d58dd612fab75eeb15af0a4fd28b
